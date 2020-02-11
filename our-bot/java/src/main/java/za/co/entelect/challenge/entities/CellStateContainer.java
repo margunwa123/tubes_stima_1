4ef0b07c7092144ff8528dcf4fc3979252f8370e
Daniel-Ri <danielriyanto190700@gmail.com>
2020-02-11 13:11:49 +0700

    Testing

--- our-bot/java/src/main/java/za/co/entelect/challenge/entities/CellStateContainer.java
+++ our-bot/java/src/main/java/za/co/entelect/challenge/entities/CellStateContainer.java
@@ -23,4 +23,8 @@ public class CellStateContainer {
     public List<Building> getBuildings(){
         return this.buildings;
     }
+
+    public List<Missile> getMissiles() {
+        return this.missiles;
+    }
 }
